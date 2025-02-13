281cfffb1928c3273178cee48b2d95667be252b0
pebloop <pebloop@outlook.com>
2025-02-13 03:36:57 +0100

    Setup initial hierarchie

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,64 @@
-<h1>Goi</h1>
-<p>The website is under construction. Please check back later.</p>
+
+<div class="text-white">
+    <div class="bg-video">
+        <div class="overlay">
+            <div class="">
+                <h1 class="text-6xl text-white">GOI</h1>
+            </div>
+        </div>
+    </div>
+
+    <div class="text-white">
+        <h2 class="text-center mt-10 text-3xl">The monster hunt is starting !</h2>
+    </div>
+
+    <div class="flex flex-row m-10 justify-evenly">
+        <img src="/learn.png" alt="learn" class="w-1/2">
+        <div class="ml-4">
+            <h2 class="text-3xl">Learn japanese</h2>
+            <p class="text-xl mt-4">Learn japanese by fighting monsters !</p>
+        </div>
+    </div>
+
+    <div class="flex flex-row m-10 mt-20 justify-evenly">
+        <div class="mr-4">
+            <h2 class="text-3xl">Explore the world</h2>
+            <p class="text-xl mt-4">Travel through the country of **** and explore its 6 regions !</p>
+        </div>
+        <img src="/country.jpg" alt="country" class="w-1/2">
+    </div>
+
+    <div class="flex flex-row m-10 mt-20 justify-evenly">
+        <img src="/monster_island.jpg" alt="mysteries" class="w-1/2">
+        <div class="ml-4">
+            <h2 class="text-3xl">Embark in an exciting adventure !</h2>
+            <p class="text-xl mt-4">Become the greatest hunter and discover some of the deepest secrets of the country.</p>
+        </div>
+    </div>
+
+    <div class="flex flex-col p-10 mt-20 bg-white text-black">
+        <h2 class="text-3xl text-center">Follow the game development on twitch !</h2>
+        <a href="https://www.twitch.tv/pebloop" class="text-center text-2xl mt-4">https://www.twitch.tv/pebloop</a>
+    </div>
+</div>
+
+<style>
+
+    .bg-video {
+        background-image: url('/goi_video.gif');
+        background-size: cover;
+        background-position: center;
+        background-attachment: fixed;
+        width: 100%;
+        height: 85vh;
+    }
+
+    .overlay {
+        background-color: rgba(0, 0, 0, 0.5);
+        width: 100%;
+        height: 100%;
+        display: flex;
+        justify-content: center;
+        align-items: center;
+    }
+</style>
