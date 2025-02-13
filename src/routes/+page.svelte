2a706f849506942b4d8a346f89957af456e54b04
pebloop <pebloop@outlook.com>
2025-02-13 02:24:47 +0100

    Setup construction message

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,2 @@
-<h1 class="">Welcome to SvelteKit</h1>
-<p>Visit <a href="https://svelte.dev/docs/kit">svelte.dev/docs/kit</a> to read the documentation</p>
+<h1>Goi</h1>
+<p>The website is under construction. Please check back later.</p>
